0219e6e9f79f9c284f5c7e326d24b7f217d201a7
MukeshDubey1420 <mukeshdubey1420@gmail.com>
2017-11-03 21:15:04 +0530

    inspect element+Developer options are  disabled...

--- main.js
+++ main.js
@@ -256,3 +256,18 @@ $('#song6').click(function() {
   
 };
 
+document.onkeydown = function(e) {
+if(event.keyCode == 123) {
+return false;
+}
+if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)){
+return false;
+}
+if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)){
+return false;
+}
+if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)){
+return false;
+}
+}
+
